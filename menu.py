d6a2a0165dee4cf7f6e20a715717aa0eb868d4b4
j_eong <tbskthddl@naver.com>
2019-02-19 12:43:37 +0900

    add checkenv script

--- menu.py
+++ menu.py
@@ -1,7 +1,16 @@
 import nuke
+import nukescripts
+import checkenv
 
 tb = nuke.toolbar("Nodes")
 m = tb.addMenu("Omiga", icon="icon_O.png")
 m.addMenu("Draw")
 m.addCommand("Draw/timecode_burnin", "nuke.createNode('timecode_burnin')")
 m.addCommand("Draw/slate", "nuke.createNode('slate')")
+
+mb = menubar.addMenu("Omiga")
+mb.addCommand("Issue_and_Bugs", "nukescripts.start('https://github.com/carmineomiga/nukeset/issues')")
+mb.addCommand("-","","")
+mb.addCommand("StartPerformanceTimers", "nuke.startPerformanceTimers()")
+mb.addCommand("StopPerformanceTimers", "nuke.stopPerformanceTimers()")
+mb.addCommand("CheckENV", "checkenv.main()")
