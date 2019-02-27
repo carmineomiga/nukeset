7ad0f6b0f2478e070c26cf21715816cf4fec39df
j_eong <tbskthddl@naver.com>
2019-02-27 09:36:05 +0900

    빠진 부분 추가

--- menu.py
+++ menu.py
@@ -3,6 +3,8 @@ import nukescripts
 import checkenv
 import openfile
 import helloworld
+import makewrite
+import nklibrary
 
 tb = nuke.toolbar("Nodes")
 m = tb.addMenu("Omiga", icon="icon_O.png")
@@ -15,6 +17,9 @@ mb.addCommand("Issue_and_Bugs", "nukescripts.start('https://github.com/carmineom
 mb.addCommand("-","","")
 mb.addCommand("StartPerformanceTimers", "nuke.startPerformanceTimers()")
 mb.addCommand("StopPerformanceTimers", "nuke.stopPerformanceTimers()")
+mb.addCommand("-","","")
 mb.addCommand("CheckENV", "checkenv.main()")
 mb.addCommand("OpenFile", "reload(openfile);openfile.main()", "F8", shortcutContext=2)
 mb.addCommand("HelloWorld", "helloworld.main()")
+mb.addCommand("MakeWrite", "reload(makewrite);makewrite.main()", "F10", shortcutContext=2)
+mb.addCommand("NkLibrary", "reload(nklibrary);nklibrary.main()")
